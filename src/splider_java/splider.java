124b3ce930d1f0ea736829d1d2c3f4709e23318d
yuyangyangyu <1813233580@qq.com>
2019-03-30 10:21:34 +0800

    更新抓取网页

--- src/splider_java/splider.java
+++ src/splider_java/splider.java
@@ -2,10 +2,10 @@ package splider_java;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class splider {
-
 	public static void main(String[] args) {
 		Document doc;
 		try{
@@ -15,22 +15,26 @@ public class splider {
 					.timeout(8000)//超时时间
 					.get();
 			String titleString=doc.title();
-			Elements element=doc.select("li.w_220").select("dt");
+			//中国热门城市（旅游）
+			Elements element=doc.select("li.w_220").select("dt");//名称
+			Elements element_1=doc.select("li.w_220").select("a[target]");//网址
+			//System.out.println(element_1.get(1).attr("href").toString());
 			for (int i = 0; i < 12; i++) {
-				String string=element.get(i).text();
+				String string_name=element.get(i).text();
+				String string_http=element_1.get(i).attr("href").toString();
 				if (i>8) {
-					System.out.println(string.substring(2));
+					System.out.println(string_name.substring(2));
 				}else {
-					System.out.println(string.substring(1));
+					System.out.println(string_name.substring(1));
 				}
-				
+				System.out.println("https://you.ctrip.com"+string_http);
 			}
 			} catch (Exception e) {//可以精确处理timeoutException
 				//超时处理
 			}	
 	
 	
- }
+	}
 
 }
 
