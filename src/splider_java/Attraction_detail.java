2b2bb8005985860f3b3f299aaf6a1c6aedb1ce99
yuyangyangyu <1813233580@qq.com>
2019-03-31 09:31:23 +0800

    新增对城市景点的详情信息的提取

--- src/splider_java/Attraction_detail.java
+++ src/splider_java/Attraction_detail.java
@@ -1,25 +1,28 @@
 package splider_java;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class splider {
-	public static void main(String[] args) {
-		Document document;
+public class Attraction_detail {
+	private Document document;
+	private Elements elements;
+	private Elements elements2;
+	/**
+	 * 获取具体景点的详情页
+	 * @param url
+	 */
+	public void Search(String url) {
 		try {
-			document= Jsoup.connect("https://you.ctrip.com/sight/chongqing158/10386.html")
+			document= Jsoup.connect(url)
 					.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:49.0) Gecko/20100101 Firefox/49.0")
 					.header("Connection", "close")//如果是这种方式，这里务必带上
 					.timeout(8000)//超时时间
 					.get();
-			//获取亮点
 			Elements elements=document.select("div.detailcon").select("ul");
-			//System.out.println(elements.text());
 			Element elements2=document.select("div.toggle_s").select("[itemprop=description]").first();
 			System.out.println(elements2.text());
 		} catch (IOException e) {
@@ -27,6 +30,19 @@ public class splider {
 			e.printStackTrace();
 		}	
 	}
+	/**
+	 * 返回景点的亮点
+	 * @return
+	 */
+public String Highlight() {
+	return elements.text();
+}
+/**
+ * 返回景点的详细情况
+ * @return
+ */
+public String detail() {
+	return elements2.text();
 }
 
-
+}
